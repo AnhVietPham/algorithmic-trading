9822b0c91ab963e61611f004d79437274d34354c
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2022-07-17 10:21:29 +0700

    cumsum.py

---
+++ data-analysis/cumsum.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+if __name__ == '__main__':
+    df = pd.DataFrame({"A": [5, 3, 6, 4],
+                       "B": [11, 2, 4, 3],
+                       "C": [4, 3, 8, 5],
+                       "D": [5, 4, 2, 8]})
+    print(df)
+    print(df.cumsum(axis=0))
+    print(df.cumsum(axis=1))
